d2615634d19293824d61c680955025ee6c81e799
Zilvinas Abromavicius <abromavicius.zilvinas@gmail.com>
2016-10-03 13:41:33 +0300

    Commented out registerActions.js, because the code in it is just an example code from another project

--- KinoPasaulis.Client/src/actions/registerActions.js
+++ KinoPasaulis.Client/src/actions/registerActions.js
@@ -1,23 +1,25 @@
-import axios from 'axios';
-import { push } from 'react-router-redux';
+//This is just an example code from another project, so it is commented out.
 
-export function register(userName, password, confirmPassword, userType) {
-  return dispatch => {
-    return axios.post('api/account/register', {
-      UserName: userName,
-      Password: password,
-      ConfirmPassword: confirmPassword,
-      UserType: userType
-    })
-      .then(response => {
-        if(response.data === true) {
-          dispatch(push('/home'));
-        }else {
-
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  }
-}
+// import axios from 'axios';
+// import { push } from 'react-router-redux';
+//
+// export function register(userName, password, confirmPassword, userType) {
+//   return dispatch => {
+//     return axios.post('api/account/register', {
+//       UserName: userName,
+//       Password: password,
+//       ConfirmPassword: confirmPassword,
+//       UserType: userType
+//     })
+//       .then(response => {
+//         if(response.data === true) {
+//           dispatch(push('/home'));
+//         }else {
+//
+//         }
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       })
+//   }
+// }

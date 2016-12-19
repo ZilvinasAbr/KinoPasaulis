bf0bd1f6d6c060935043946dc798a01eaf4fa42d
Zilvinas Abromavicius <abromavicius.zilvinas@gmail.com>
2016-12-20 00:07:53 +0200

    Fixed home page

--- KinoPasaulis.Client/src/components/home/CinemaStudioHomePage.js
+++ KinoPasaulis.Client/src/components/home/CinemaStudioHomePage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+
 import CinemaStudioNavigationBar from '../../cinemaStudio/components/CinemaStudioNavigationBar';
 
 class CinemaStudioHomePage extends React.Component {
@@ -10,7 +12,13 @@ class CinemaStudioHomePage extends React.Component {
     return (
       <div>
         <CinemaStudioNavigationBar />
-        <h1> Cinema Studio Home Page </h1>
+        <Grid>
+          <Row>
+            <Col xs={8} xsOffset={2} sm={8} smOffset={2} md={6} mdOffset={3} lg={6} lgOffset={3}>
+            <h1>Sveiki prisijungę!</h1>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }

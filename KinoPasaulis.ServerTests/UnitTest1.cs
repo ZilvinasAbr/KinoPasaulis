b2d2392ccce97ec7b80def9fe17641125cb83af1
Zilvinas Abromavicius <abromavicius.zilvinas@gmail.com>
2017-12-01 15:00:05 +0200

    Add xUnit project.

---
+++ KinoPasaulis.ServerTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace KinoPasaulis.ServerTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

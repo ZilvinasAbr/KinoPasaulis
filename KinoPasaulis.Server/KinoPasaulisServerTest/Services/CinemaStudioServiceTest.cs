57eea389ab74bfd92c77f8efd7fcd5dd76deca2f
Zilvinas Abromavicius <abromavicius.zilvinas@gmail.com>
2017-12-01 19:48:31 +0200

    Create some tests, and a mock

---
+++ KinoPasaulis.Server/KinoPasaulisServerTest/Services/CinemaStudioServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using KinoPasaulis.Server.Services;
+using KinoPasaulisServerTest.Repositories.CinemaStudio;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Xunit;
+
+namespace KinoPasaulisServerTest.Services
+{
+    public class CinemaStudioServiceTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var movieRepositoryMock = new MovieRepositoryMock();
+            var cinemaStudioService = new CinemaStudioService(movieRepositoryMock, null, new HostingEnvironment());
+
+            Assert.True(cinemaStudioService != null);
+        }
+    }
+}
